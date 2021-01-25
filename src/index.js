d3def3510c320955f99e354efcde28c10555548f
Marthely <jeadestetsiba@gmail.com>
2021-01-25 19:58:53 +0100

    adding package todo mvc css and remove readme file from git tracked files

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { render } from 'react-dom'
 import App from './containers/App'
 import 'todomvc-app-css/index.css'
 import registerServiceWorker from './registerServiceWorker'
+import 'todomvc-app-css/index.css'
 
 render(
 	<App />,
