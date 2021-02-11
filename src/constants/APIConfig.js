4653f67a5c0478f68cb24abdecbf93f3213f732a
Marthely <jeadestetsiba@gmail.com>
2021-02-11 17:19:42 +0100

    we're going to use an api ;-)

---
+++ src/constants/APIConfig.js
@@ -0,0 +1,6 @@
+export const HEADERS = new Headers({
+	'Content-Type': 'application/json',
+	Accept: 'application/json',
+	Authorization: '90f488077927d9351ce7d726f992142b2IbEkWlHr2CwvBr5sJW1o9YSmVMyPnQvep+2ioUNKW4TT/afEYon+NaxCjs2hm/R',
+})
+export const API_URI = 'http://localhost:3333/api'
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
  
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './components/App'
 import reducer from './reducers'
 import 'todomvc-app-css/index.css'
 import registerServiceWorker from './registerServiceWorker'
+import thunk from 'redux-thunk'
 
-const store = createStore(reducer)
+const store = createStore(
+    reducer,
+    applyMiddleware(thunk)
+)
 
 render(
 	<Provider store={store}>
